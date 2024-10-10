9dae45ffb711d308ef17599595eb1033aed5ce7d
Pavan <pavankumaroggu31@gmail.com>
2024-10-10 20:30:00 +0530

    even check code added

--- even.js
+++ even.js
@@ -1,6 +1,5 @@
 function evenNumber(num){
-    let num=[2,7,12,15,16,48,57,42];
-    for(let i=0;i<num.length;i++){
+    for(let i=0;i<10;i++){
         if(i%2==0){
             console.log(i);
         }
